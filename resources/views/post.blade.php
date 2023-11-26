4a8b5ab11504ad689713a9a7e5e43bb843c3ed78
DerralPramudya <derralpramudia8@gmail.com>
2023-11-26 14:07:35 +0700

    vid8

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,10 +1,7 @@
-@extends('layouts.main')
-
 @section('container')
 <article>
     <h2>{{ $post->title }}</h2>
+
+    <p>By. Derral Pramudya<a href="/categories/{{ $post->category->slug }}">{{ $post->category->name}}</a></p>
     {!! $post->body !!}
 </article>
-
-<a href="/blog">Back to Posts</a>
-@endsection 
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -22,11 +22,10 @@ Route::get('/', function () {
     ]);
 });
 
-
 Route::get('/about', function () {
     return view('about',[
         "title" => "About",
-        "name" => "M Derral P", 
+        "name" => "M Derral P",
         "email" => "derralgaming99@gmail.com",
         "image" => "Derral.jpeg"
     ]);
@@ -35,5 +34,20 @@ Route::get('/about', function () {
 
 Route::get('/blog',[PostController::class, 'index']);
 
-//Halaman Single Post
+//halaman single post
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('categories', function() {
+    return view('categories', [
+        'title' => 'Post Categories',
+        'categories' => Category::all()
+    ]);
+});
+
+Route::get('/categories/{category:slug}', function(Category $category) {
+    return view('category', [
+        'title' => $category->name,
+        'posts' => $category->posts,
+        'category' => $category->name
+    ]);
+});
