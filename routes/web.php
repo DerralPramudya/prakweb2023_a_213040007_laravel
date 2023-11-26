e8dd0f44cfb439e7f94191893726dd17835d6785
DerralPramudya <derralpramudia8@gmail.com>
2023-11-26 12:41:58 +0700

    update Video 2

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about',[
+        "name" => "M Derral P",
+        "email" => "derralgaming99@gmail.com",
+        "image" => "Derral.jpeg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
